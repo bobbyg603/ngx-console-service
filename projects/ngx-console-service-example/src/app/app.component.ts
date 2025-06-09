64686dfd9600fa64955ee61d8fe661c7864afea3
Bobby Galli <bobbyg603@pm.me>
2025-06-09 14:01:03 -0400

    chore: update angular again

--- projects/ngx-console-service-example/src/app/app.component.ts
+++ projects/ngx-console-service-example/src/app/app.component.ts
@@ -3,11 +3,10 @@ import { RouterOutlet } from '@angular/router';
 import { NgxConsoleService } from '../../../ngx-console-service/src/public-api';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   readonly console = inject(NgxConsoleService);
