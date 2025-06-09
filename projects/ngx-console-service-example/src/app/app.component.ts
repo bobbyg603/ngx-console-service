3631a0b8c370b80fded23c3b1efd8898b1129443
Bobby Galli <bobbyg603@pm.me>
2025-06-09 14:06:52 -0400

    fix: demo

--- projects/ngx-console-service-example/src/app/app.component.ts
+++ projects/ngx-console-service-example/src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NgxConsoleService } from '../../../ngx-console-service/src/public-api';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   readonly console = inject(NgxConsoleService);
-  
+
   log() {
     this.console.log('log');
   }
